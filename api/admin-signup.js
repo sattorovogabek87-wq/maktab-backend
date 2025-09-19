5daf564bda7f85c458dc74308f574c9d95d2e1cd
sattorovogabek87-wq <sattorovogabek87@gmail.com>
2025-09-19 22:40:34 +0500

    admin-signup.js

--- api/admin-signup.js
+++ api/admin-signup.js
@@ -6,7 +6,7 @@ const SUPABASE_SERVICE_ROLE_KEY =
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Faqat POST so‘rovi ishlaydi!" });
+    return res.status(405).json({ error: "Faqat POST so‘rovi ishlaydi bratishka!" });
   }
 
   const {
@@ -69,4 +69,5 @@ export default async function handler(req, res) {
     .status(200)
     .json({ success: true, message: "Yangi user yaratildi!" });
 }
+
 
